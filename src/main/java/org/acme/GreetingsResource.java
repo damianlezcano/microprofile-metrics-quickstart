947fa8095534dbe577d59566b99990975729b1d9
Juan Lozano <jlozano@redhat.com>
2023-01-04 10:30:14 -0300

    Added stdout print to test EFK Stack

--- src/main/java/org/acme/GreetingsResource.java
+++ src/main/java/org/acme/GreetingsResource.java
@@ -13,10 +13,16 @@ public class GreetingsResource {
     @Inject
     MeterRegistry registry;
 
+    private int requests = 0;
+
     @GET
     @Path("/{name}")
     public String sayHello(@PathParam(value = "name") String name) {
         registry.counter("greeting_counter", Tags.of("name", name)).increment();
+
+        // Fot test stdout in Kibana (EFK Stack)
+        requests++;
+        System.out.println(String.format("Received request %d with parameter: %s", requests, name));
 
         return String.format("Hello %s!", name);
     }
